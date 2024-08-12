03c85808d7b4585448dac9104bcb4701562bf479
BaoTrung37 <baotrung372k@gmail.com>
2024-08-12 21:26:57 +0700

    init web

--- lib/domain/use_cases/onboarding/onboarding.dart
+++
@@ -1,2 +0,0 @@
-export 'get_first_time_launch_use_case.dart';
-export 'post_first_time_launch_use_case.dart';
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:baotrung37_github_io/main.dart';
+import 'package:flutter_portfolio/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
