23ecdfe46884c02291331f0ecbefabf6484b6377
Levi <levi@blueverse.kr>
2024-08-13 17:50:21 +0700

    Update home_screen.dart
    Co-Authored-By: Nguyễn Bảo Trung <baotrung372k@gmail.com>

--- lib/presentation/pages/home/screens/home_screen.dart
+++ lib/presentation/pages/home/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/config/color/app_colors.dart';
 import 'package:flutter_portfolio/presentation/presentation.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 @RoutePage()
 class HomeScreen extends StatelessWidget {
@@ -9,6 +10,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: context.colors.backgroundColor,
       body: SingleChildScrollView(
@@ -19,6 +21,35 @@ class HomeScreen extends StatelessWidget {
               style: AppTextStyles.headingLarge.copyWith(
                 color: context.colors.textPrimary,
               ),
+            ),
+            ResponsiveRowColumn(
+              layout: ResponsiveBreakpoints.of(context).isDesktop
+                  ? ResponsiveRowColumnType.ROW
+                  : ResponsiveRowColumnType.COLUMN,
+              columnCrossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ResponsiveRowColumnItem(
+                  child: Container(
+                    width: width * 0.24,
+                    height: 300,
+                    color: Colors.red,
+                  ),
+                ),
+                ResponsiveRowColumnItem(
+                  child: Container(
+                    width: width * 0.24,
+                    height: 300,
+                    color: Colors.green,
+                  ),
+                ),
+                ResponsiveRowColumnItem(
+                  child: Container(
+                    width: width * 0.24,
+                    height: 300,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
